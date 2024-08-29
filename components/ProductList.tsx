6b5009d09c10d3bc295222f10d45beaa5a2529b3
nazimuddin <nazimuddin102001@gmail.com>
2024-08-29 23:39:25 +0600

    implemented single item page design

---
+++ components/ProductList.tsx
@@ -0,0 +1,185 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const ProductList = () => {
+  return (
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+      <Link
+        href={'/test'}
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src={
+              'https://images.pexels.com/photos/20218545/pexels-photo-20218545/free-photo-of-pancakes-on-a-plate.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            }
+            alt="imageOne"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+
+          <Image
+            src={
+              'https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800'
+            }
+            alt="imageOne"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">name</span>
+          <span className="font-semibold">$price</span>
+        </div>
+        <div className="text-gray-500 text-sm my-2">Description</div>
+        <button className="rounded-2xl ring-1 ring-nazim text-nazim w-max py-2 px-4 text-xs hover:bg-nazim hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+
+      <Link
+        href={'/test'}
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src={
+              'https://images.pexels.com/photos/20218545/pexels-photo-20218545/free-photo-of-pancakes-on-a-plate.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            }
+            alt="imageOne"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+
+          <Image
+            src={
+              'https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800'
+            }
+            alt="imageOne"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">name</span>
+          <span className="font-semibold">$price</span>
+        </div>
+        <div className="text-gray-500 text-sm my-2">Description</div>
+        <button className="rounded-2xl ring-1 ring-nazim text-nazim w-max py-2 px-4 text-xs hover:bg-nazim hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+
+      <Link
+        href={'/test'}
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src={
+              'https://images.pexels.com/photos/20218545/pexels-photo-20218545/free-photo-of-pancakes-on-a-plate.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            }
+            alt="imageOne"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+
+          <Image
+            src={
+              'https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800'
+            }
+            alt="imageOne"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">name</span>
+          <span className="font-semibold">$price</span>
+        </div>
+        <div className="text-gray-500 text-sm my-2">Description</div>
+        <button className="rounded-2xl ring-1 ring-nazim text-nazim w-max py-2 px-4 text-xs hover:bg-nazim hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+
+      <Link
+        href={'/test'}
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src={
+              'https://images.pexels.com/photos/20218545/pexels-photo-20218545/free-photo-of-pancakes-on-a-plate.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            }
+            alt="imageOne"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+
+          <Image
+            src={
+              'https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800'
+            }
+            alt="imageOne"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">name</span>
+          <span className="font-semibold">$price</span>
+        </div>
+        <div className="text-gray-500 text-sm my-2">Description</div>
+        <button className="rounded-2xl ring-1 ring-nazim text-nazim w-max py-2 px-4 text-xs hover:bg-nazim hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+
+      <Link
+        href={'/test'}
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src={
+              'https://images.pexels.com/photos/20218545/pexels-photo-20218545/free-photo-of-pancakes-on-a-plate.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            }
+            alt="imageOne"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+
+          <Image
+            src={
+              'https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800'
+            }
+            alt="imageOne"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">name</span>
+          <span className="font-semibold">$price</span>
+        </div>
+        <div className="text-gray-500 text-sm my-2">Description</div>
+        <button className="rounded-2xl ring-1 ring-nazim text-nazim w-max py-2 px-4 text-xs hover:bg-nazim hover:text-white">
+          Add to Cart
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductList;
